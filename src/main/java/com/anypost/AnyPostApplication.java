f7e095f9da32318ae46758e7169ebad39be7e437
ClickInPasto <landingpageclickin@gmail.com>
2025-10-15 10:55:26 -0500

    Cargando Spring Boot

---
+++ src/main/java/com/anypost/AnyPostApplication.java
@@ -0,0 +1,13 @@
+package com.anypost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyPostApplication.class, args);
+	}
+
+}
